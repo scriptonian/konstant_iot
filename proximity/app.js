ccf33db70bc2b0810ab3e028ce46559ce963a80e
I-am Konstantin <Konstantin@i-ams-mbp.home>
2017-01-28 21:22:42 -0500

    adding proximity project

---
+++ proximity/app.js
@@ -0,0 +1,15 @@
+var Gpio = require('onoff').Gpio,
+    sensor = new Gpio(17, 'in', 'both');
+
+sensor.watch(function(err, value) {
+  if (err) exit(err);
+  console.log(value ? 'there is some one!' : 'not anymore');
+});
+
+function exit(err) {
+  if (err) console.log('An error occurred: ' + err);
+  sensor.unexport();
+  console.log('Bye, bye!');
+  process.exit();
+}
+process.on('SIGINT', exit);
