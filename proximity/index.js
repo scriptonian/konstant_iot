912f8c854ded38f02cc559308b870d407232035e
I-am Konstantin <Konstantin@i-ams-mbp.home>
2017-01-28 22:56:11 -0500

    add gpio version of code instead

--- proximity/index.js
+++ proximity/index.js
@@ -1,4 +1,28 @@
-var Gpio = require('onoff').Gpio,
+var gpio = require("pi-gpio");
+
+pin = 11;
+
+function readProximity() {
+  gpio.open(pin, "input", function (err) { //#A
+    gpio.read(pin, function (err, value) { //#B
+      if (err) exit(err);
+      console.log(value ? 'there is some one!' : 'not anymore!'); //#C
+      readProximity();
+    });
+  });
+}
+
+function exit(err) {
+  gpio.close(pin);
+  if (err) console.log('An error occurred: ' + err);
+  console.log('Bye, bye!')
+  process.exit();
+}
+process.on('SIGINT', exit);
+
+readProximity();
+
+/*var Gpio = require('onoff').Gpio,
   sensor = new Gpio(17, 'in', 'both');    //#A
 
 console.log('yo');
@@ -15,3 +39,4 @@ function exit(err) {
   process.exit();
 }
 process.on('SIGINT', exit);
+*/
